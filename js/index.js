e517e1f4d2d3640943905ec42ba2e02d66c7120a
scott-buttocolla@lambdastudents.com <sbuttocolla0810@gmail.com>
2020-10-26 20:20:10 -0700

    finished MVP for the most, added extra styling

--- js/index.js
+++ js/index.js
@@ -58,74 +58,115 @@ const blogLink = document.createElement("a");
 blogLink.textContent = "Blog"
 blogLink.href = "#";
 document.querySelector("nav").appendChild(blogLink);
-
 const visitLink =document.createElement("a");
 visitLink.textContent = "Visit";
 visitLink.href = "#";
 document.querySelector("nav").prepend(visitLink);
 
-// link colors
-console.log(navLinks)
-const navColor = document.querySelectorAll("nav a");
-console.log(navColor)
-//navColor.style.color ="green";
 
-// .style.color = "green";
+// const navStyle = document.querySelectorAll("nav a");
+// navLinks.forEach((item, i) => {
+//   item.style.color = "green"
+// })  works but does not included 2 added links with append and prepend
+
+// link colors
+document.querySelectorAll('a').forEach(e => e.style.color = "green");
+// link colors
 
 const logoh1 = document.querySelector('.cta-text h1');
 logoh1.textContent = 'DOM Is Awesome';
-logoh1.style.color = "green";
+logoh1.style.color = "darkblue";
+document.querySelectorAll('h1').forEach(c => c.style.fontSize = "7rem")
 
 const startButton = document.querySelector('.cta .cta-text button');
 startButton.textContent = "Get Started";
+document.querySelectorAll('button').forEach(b => b.style.borderRadius = "50%")
+startButton.style.textDecoration = "underline"
 
 //  Above picture content
-const featuresH4 = document.querySelector('.top-content:nth-child(1) h4 ');
-featuresH4.textContent = "Features";
+// const featuresHeader = document.querySelectorAll('.text-content h4');
+// featuresHeader[0].textContent = siteContent['main-content']['features-h4']
+// const featuresContent = document.querySelectorAll('.text-content p');
+// featuresContent[0].textContent = siteContent['main-content']['features-content']
 
-const featuresText = document.querySelector('.top-content:nth-child(1) p');
-featuresText.textContent = '"Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."'
+const featureHeader = document.querySelectorAll('.text-content h4');
+featureHeader[0].textContent = siteContent['main-content']['features-h4']
+const featuresText = document.querySelectorAll('.text-content p');
+featuresText[0].textContent = siteContent['main-content']['features-content']
 
-const aboutH4 = document.querySelector('.top-content .text-content:nth-child(2) h4 ');
-aboutH4.textContent = "About";
-const aboutText = document.querySelector('.text-content:nth-child(2) p');
-aboutText.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+
+// const featuresH4 = document.querySelector('.top-content:nth-child(1) h4 ');
+// featuresH4.textContent = "Features";
+// const featuresText = document.querySelector('.top-content:nth-child(1) p');
+// featuresText.textContent = '"Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."'
+
+const aboutHeader = document.querySelectorAll('.text-content h4');
+aboutHeader[1].textContent = siteContent['main-content']['about-h4']
+const aboutContent = document.querySelectorAll('.text-content p');
+aboutContent[1].textContent = siteContent['main-content']['about-content']
+
+// const aboutText = document.querySelector('.text-content:nth-child(2) p');
+// aboutText.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+// const aboutH4 = document.querySelector('.top-content .text-content:nth-child(2) h4 ');
+// aboutH4.textContent = "About";
+// const aboutText = document.querySelector('.text-content:nth-child(2) p');
+// aboutText.textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
 
 
 
 // below picture content
+const serviceHeader = document.querySelectorAll('.text-content h4');
+serviceHeader[2].textContent = siteContent['main-content']['services-h4']
+const serviceContent = document.querySelectorAll('.text-content p');
+serviceContent[2].textContent = siteContent['main-content']['services-content'];
+// const servicesH4 = document.querySelector('.bottom-content .text-content:nth-child(1) h4');
+// servicesH4.textContent = "Services";
+// const servicesText = document.querySelector('.bottom-content .text-content:nth-child(1) p');
+// servicesText.textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+const productHeader = document.querySelectorAll('.text-content h4');
+productHeader[3].textContent = siteContent['main-content']['product-h4'];
+const productContent =document.querySelectorAll('.text-content p');
+productContent[3].textContent = siteContent['main-content']['product-content'];
+// const productH4 = document.querySelector('.bottom-content .text-content:nth-child(2) h4 ');
+// productH4.textContent = "Product";
+// const productText = document.querySelector('.bottom-content .text-content:nth-child(2) p');
+// productText.textContent = " Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+const visionHeader = document.querySelectorAll('.text-content h4');
+visionHeader[4].textContent = siteContent['main-content']['vision-h4'];
+const visionContent = document.querySelectorAll('.text-content p');
+visionContent[4].textContent = siteContent['main-content']['vision-content'];
+// const visionH4 = document.querySelector(".bottom-content .text-content:nth-child(3) h4")
+// visionH4.textContent = "Vision";
+// const visionText = document.querySelector(".bottom-content .text-content:nth-child(3) p")
+// visionText.textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
 
-const servicesH4 = document.querySelector('.bottom-content .text-content:nth-child(1) h4');
-servicesH4.textContent = "Services";
-const servicesText = document.querySelector('.bottom-content .text-content:nth-child(1) p');
-servicesText.textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
-
-const productH4 = document.querySelector('.bottom-content .text-content:nth-child(2) h4 ');
-productH4.textContent = "Product";
-const productText = document.querySelector('.bottom-content .text-content:nth-child(2) p');
-productText.textContent = " Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
-
-const visionH4 = document.querySelector(".bottom-content .text-content:nth-child(3) h4")
-visionH4.textContent = "Vision";
-const visionText = document.querySelector(".bottom-content .text-content:nth-child(3) p")
-visionText.textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
 
 // Contact information
+const contactHeader = document.querySelector('.contact h4');
+contactHeader.textContent = siteContent['contact']['contact-h4']
+// const contactH4 = document.querySelector(".contact h4");
+// contactH4.textContent = "Contact";
+const contactText = document.querySelectorAll('.contact p');
+contactText[0].textContent = siteContent['contact']['address']
+// var test = document.getElementById('someElementId');
+// test.innerHTML = "The answer <br>to life, the universe, and everything...<br> is 42.";
+// const address = document.querySelector(".contact p:nth-of-type(1)");
+// address.textContent = "123 Way 456 Street Somewhere, USA"
+const contactNum = document.querySelectorAll('.contact p');
+contactNum[1].textContent = siteContent['contact']['phone']
+// const phoneNum = document.querySelector(".contact p:nth-of-type(2)");
+// phoneNum.textContent = "1 (888) 888-8888";
+const contactEmail = document.querySelectorAll('.contact p');
+contactEmail[2].textContent = siteContent['contact']['email']
+// const emailAdd = document.querySelector(".contact p:nth-of-type(3)");
+// emailAdd.textContent = "sales@greatidea.io"
 
-const contactH4 = document.querySelector(".container .contact h4");
-contactH4.textContent = "Contact";
-
-const address = document.querySelector(".container .contact p:nth-of-type(1)");
-address.textContent = "123 Way 456 Street Somewhere, USA"
-
-const phoneNum = document.querySelector(".container .contact p:nth-of-type(2)");
-phoneNum.textContent = "1 (888) 888-8888";
-
-const emailAdd = document.querySelector(".container .contact p:nth-of-type(3)");
-emailAdd.textContent = "sales@greatidea.io"
+document.querySelectorAll('h4').forEach(e => e.style.color = "blue");
+document.querySelectorAll('h4').forEach(q => q.style.fontSize = "2rem")
 
 
-
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//Styles all elements inside the ('???')
 
 
 const footer = document.querySelector('footer p')
